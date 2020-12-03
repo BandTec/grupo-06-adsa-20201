ed1026fcd87acbd1f1cefb481b8ff3337d775d86
caiohmgithub <caio.martins@bandtec.com.br>
2020-12-03 18:54:46 -0300

    Link Slack Comentado

--- OSHI/OSHI-MACHINETECH/src/main/java/br/com/bandtec/slack/api/SendMessage.java
+++ OSHI/OSHI-MACHINETECH/src/main/java/br/com/bandtec/slack/api/SendMessage.java
@@ -11,7 +11,7 @@ package br.com.bandtec.slack.api;
 
     public class SendMessage {
 
-        private static String slackWebhookUrl = "https://hooks.slack.com/services/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(remove-isso-pra-continuar)";
+//        private static String slackWebhookUrl = "https://hooks.slack.com/services/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";
 
         public static void sendMessage(Message message) {
             CloseableHttpClient client = HttpClients.createDefault();
